5405537211c0a6c564a47c966722bac285eb3cad
Rohit Chakravarti <rohitchakravarti2002@gmail.com>
2026-05-24 15:06:45 +0530

    Day 10 - React Setup JSX Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+function Header() {
+  return (
+    <div>
+      <h1>Mera App!</h1>
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+// 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;  */
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {}
+      <h1 className="title">Hello!</h1>
+
+      {}
+      <p>{2 + 2}</p>
+
+      {}
+      <p>Paragraph 1</p>
+      <p>Paragraph 2</p>
     </div>
   );
 }
